49ee45da25d3229b76301390c67455953b9bef0a
Juan P. Rodríguez B <jrodriguezbr@unal.edu.co>
2025-06-09 11:23:34 -0500

    LaTeX to string works properly

--- logica.cs
+++ logica.cs
@@ -1,30 +1,35 @@
 using System;
 using System.Collections.Generic;
 class Logica {
-    
-  static void Main() {
-      
-     Dictionary<string, string> latexSyntax = new Dictionary<string, string>();
-      
-    latexSyntax.Add ("¬", "\\neg");
-    latexSyntax.Add ("Y", "\\land");
-    latexSyntax.Add ("O", "\\lor");
-    latexSyntax.Add ("->","\\implies");
-    latexSyntax.Add ("<->","\\iff");
-    
+
+  static void Main()
+  {
+
+    Dictionary<string, string> latexSyntax = new Dictionary<string, string>();
+
+    latexSyntax.Add("¬", "\\neg");
+    latexSyntax.Add("Y", "\\land");
+    latexSyntax.Add("O", "\\lor");
+    latexSyntax.Add("->", "\\implies");
+    latexSyntax.Add("<->", "\\iff");
+
     Console.WriteLine("Hello World");
-    
+
     Console.WriteLine("Enter LaTeX expression:");
     string latexString = Console.ReadLine();
     latexString = latexString.ToLower();
-    
-    foreach (var latexOperator in latexSyntax) 
-    {  
-        latexString = latexString.Replace(latexOperator.Value ,latexOperator.Key);
-        Console.WriteLine(latexOperator);
+    Console.WriteLine("The LaTeX string is:"+ latexString);
+
+    string normalString = latexString;
+
+    // Replace LaTeX operators with their corresponding symbols
+    foreach (var latexOperator in latexSyntax)
+    {
+      normalString = normalString.Replace(latexOperator.Value, latexOperator.Key);
+      Console.WriteLine(latexOperator);
     }
+
+    Console.WriteLine("The base string is: " + normalString);
     
-    
-    Console.WriteLine(latexString);
   }
 }
