1966d297944540e1921fc1e2f7965b4f93ca4dde
Juan P. Rodríguez B <jrodriguezbr@unal.edu.co>
2025-06-09 01:57:22 -0500

    String replacement

--- logica.cs
+++ logica.cs
@@ -1,32 +1,30 @@
 using System;
 using System.Collections.Generic;
-class HelloWorld
-{
-
-
-
-    static void Main()
-    {
-
-        Dictionary<string, string> latexSyntax = new Dictionary<string, string>();
-
-        latexSyntax.Add("¬", "\\neg");
-        latexSyntax.Add("Y", "\\land");
-        latexSyntax.Add("O", "\\lor");
-        latexSyntax.Add("->", "\\implies");
-        latexSyntax.Add("<->", "\\iff");
-
-        Console.WriteLine("Hello World");
-
-
-
-        foreach (var pair in latexSyntax)
-        {
-            Console.WriteLine(pair);
-        }
-
-    }
-}
-  
-  
+class Logica {
     
+  static void Main() {
+      
+     Dictionary<string, string> latexSyntax = new Dictionary<string, string>();
+      
+    latexSyntax.Add ("¬", "\\neg");
+    latexSyntax.Add ("Y", "\\land");
+    latexSyntax.Add ("O", "\\lor");
+    latexSyntax.Add ("->","\\implies");
+    latexSyntax.Add ("<->","\\iff");
+    
+    Console.WriteLine("Hello World");
+    
+    Console.WriteLine("Enter LaTeX expression:");
+    string latexString = Console.ReadLine();
+    latexString = latexString.ToLower();
+    
+    foreach (var latexOperator in latexSyntax) 
+    {  
+        latexString = latexString.Replace(latexOperator.Value ,latexOperator.Key);
+        Console.WriteLine(latexOperator);
+    }
+    
+    
+    Console.WriteLine(latexString);
+  }
+}
